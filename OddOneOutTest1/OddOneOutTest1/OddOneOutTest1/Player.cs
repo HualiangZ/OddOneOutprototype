a64a583ab64b40494c0853e8c35aa7f8ddbd6993
HualiangZ <103204135+HualiangZ@users.noreply.github.com>
2022-05-08 12:29:17 +0100

    prototype code for odd one out

---
+++ OddOneOutTest1/OddOneOutTest1/OddOneOutTest1/Player.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OddOneOutTest1
+{
+    class Player
+    {
+        String name;
+        int votes;
+        public Player(String name)
+        {
+            this.name = name;
+            votes = 0;
+        }
+
+        public String Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+
+        public void addVote()
+        {
+            votes += 1;
+        }
+
+        public int getVote
+        {
+            get { return votes; }
+        }
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
---
+++ OddOneOutTest1/OddOneOutTest1/OddOneOutTest1/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace OddOneOutTest1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] animal = { "Cat", "Dog", "Mouse" };
+            Random rand = new Random();
+               
+            int x = rand.Next(0, 3);
+
+
+            List<Player> players = new List<Player>();
+            Boolean a = true;
+            while(a == true)
+            {
+                Console.WriteLine("enter name");
+                string name = Console.ReadLine();
+                if(name != " ")
+                {
+                    Player player = new Player(name);
+                    players.Add(player);
+                }
+                else
+                {
+                    a = false;
+                }
+            }
+
+
+            int y = rand.Next(0, 5);
+
+            for (int i = 0; i < players.Count; i++)
+            {
+                if (i != y)
+                    Console.WriteLine(players[i].Name.ToString() + animal[x]);
+                else
+                    Console.WriteLine(players[i].Name + " Guess the animal");
+            }
+
+
+            for (int i = 0; i < players.Count; i++)
+            {
+                Console.WriteLine(players[i].Name + " Vote for a player");
+                string guess = Console.ReadLine();
+                for (int j = 0; j < players.Count; j++)
+                {
+                    if (guess == players[j].Name.ToString())
+                    {
+                        if (players[i].Name == players[j].Name || guess == null)
+                        {
+                            Console.WriteLine("cant vote for yourself");
+                            i -= 1;
+                            
+                        }
+                        else
+                        {
+                            players[j].addVote();
+                        }
+
+                    }
+
+                }
+            }
+
+            for (int i = 0; i < players.Count; i++)
+            {
+                Console.WriteLine(players[i].Name.ToString() + " votes: " + players[i].getVote);
+
+            }
+
+            int max = players[0].getVote;
+            
+            for(int i =0; i<players.Count; i++)
+            {
+                int value = players[i].getVote;
+                if (value > max)
+                    max = value; 
+                
+            }
+            Console.WriteLine(max);
+
+        }
+    }
+}
